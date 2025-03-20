e361f8ffac0659601b4f4aee8281473d564f0334
Dave Benson <dave.benson@embodied.me>
2025-03-20 11:20:42 -0700

    homepage

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+
+<template>
+  <div>
+    <center>About Me</center>
+    <p>Experienced professional with a strong background in technology-related roles. Proficient in software development and system administration. Skilled in problem-solving and optimizing performance. Capable of spearheading projects and collaborating effectively with teams. Committed to continuous learning and staying current with industry trends to contribute to organizational success.</p>
+  </div>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AboutMe from '../components/AboutMe.vue'
+</script>
+
+<template>
+  <main>
+    <AboutMe />
+  </main>
+</template>
